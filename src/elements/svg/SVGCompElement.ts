2a56b7dfea39bd75be8eb807999db69bc1b7ea58
Johan Martin Aarstein <johan@aarstein.media>
2025-03-31 12:03:06 +0200

    inc

--- src/elements/svg/SVGCompElement.ts
+++ src/elements/svg/SVGCompElement.ts
@@ -9,7 +9,6 @@ import type { KeyframedValueProperty } from '@/utils/Properties'
 import CompElement from '@/elements/CompElement'
 import SVGBaseElement from '@/elements/svg/SVGBaseElement'
 import SVGRendererBase from '@/renderers/SVGRendererBase'
-import { extendPrototype } from '@/utils/functionExtensions'
 import { createSizedArray } from '@/utils/helpers/arrays'
 import PropertyFactory from '@/utils/PropertyFactory'
 
@@ -33,21 +32,30 @@ export default class SVGCompElement extends SVGBaseElement {
     this.pendingElements = []
     this.elements = this.layers ? createSizedArray(this.layers.length) : []
     const {
-      appendElementInPos,
-      buildItem,
-      checkPendingElements,
-      configAnimation,
-      createImage,
-      createNull,
-      createShape,
-      createSolid,
-      createText,
-      destroy,
-      findIndexByInd,
-      hide,
-      // renderFrame,
-      // show,
-    } = new SVGRendererBase()
+        appendElementInPos,
+        buildItem,
+        checkPendingElements,
+        configAnimation,
+        createImage,
+        createNull,
+        createShape,
+        createSolid,
+        createText,
+        // destroy,
+        findIndexByInd,
+        hide,
+        // renderFrame,
+        // show,
+      } = new SVGRendererBase(),
+      {
+        destroy,
+        destroyElements,
+        getElements,
+        initElement,
+        prepareFrame,
+        renderInnerContent,
+        setElements,
+      } = new CompElement()
     this.appendElementInPos = appendElementInPos
     this.buildItem = buildItem
     this.checkPendingElements = checkPendingElements
@@ -58,8 +66,14 @@ export default class SVGCompElement extends SVGBaseElement {
     this.createSolid = createSolid
     this.createText = createText
     this.destroy = destroy
+    this.destroyElements = destroyElements
     this.findIndexByInd = findIndexByInd
     this.hide = hide
+    this.getElements = getElements
+    this.initElement = initElement
+    this.prepareFrame = prepareFrame
+    this.renderInnerContent = renderInnerContent
+    this.setElements = setElements
     // this.renderFrame = renderFrame
     // this.show = show
     this.initElement(data, globalData, comp)
@@ -141,15 +155,45 @@ export default class SVGCompElement extends SVGBaseElement {
     )
   }
 
+  destroyElements() {
+    throw new Error(
+      `${this.constructor.name}: Method destroyElements is not impelemented`
+    )
+  }
+
   findIndexByInd(_ind: number) {
     throw new Error(
       `${this.constructor.name}: Method findIndexByInd is not impelemented`
     )
   }
+
+  getElements() {
+    throw new Error(
+      `${this.constructor.name}: Method getElements is not impelemented`
+    )
+  }
+
+  setElements(_elems: ElementInterfaceIntersect[]) {
+    throw new Error(
+      `${this.constructor.name}: Method setElements is not impelemented`
+    )
+  }
 }
 
-for (const proto of Object.getOwnPropertyNames(SVGRendererBase.prototype)) {
-  console.log(proto)
-}
+// const props = Object.getOwnPropertyNames(CompElement.prototype),
+//   sorted = props.sort((a, b) => {
+//     if (a < b) {
+//       return -1
+//     }
+//     if (a > b) {
+//       return 1
+//     }
+//     return 0
+//   })
 
-extendPrototype([CompElement], SVGCompElement)
+// for (const s of sorted) {
+//   if (s === 'constructor') {
+//     continue
+//   }
+//   console.log(s)
+// }
