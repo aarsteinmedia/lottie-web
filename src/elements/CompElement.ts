b342fb4f2ce57e9961f99a812103689600683d04
Johan Martin Aarstein <johan@aarstein.media>
2025-03-31 12:32:17 +0200

    inc

--- src/elements/CompElement.ts
+++ src/elements/CompElement.ts
@@ -4,56 +4,39 @@ import type {
   LottieLayer,
 } from '@/types'
 
-import FrameElement from '@/elements/helpers/FrameElement'
-import HierarchyElement from '@/elements/helpers/HierarchyElement'
 import RenderableDOMElement from '@/elements/helpers/RenderableDOMElement'
-import TransformElement from '@/elements/helpers/TransformElement'
-import BaseRenderer from '@/renderers/BaseRenderer'
-// import SVGRendererBase from '@/renderers/SVGRendererBase'
-import { extendPrototype } from '@/utils/functionExtensions'
 import { ValueProperty } from '@/utils/Properties'
 
-export default class CompElement extends BaseRenderer {
-  // _mdf?: boolean
-  createContainerElements: any
-  createRenderableComponents: any
-  initRendererElement: any
-  isInRange?: boolean
-
+export default class CompElement extends RenderableDOMElement {
+  completeLayers?: boolean
+  elements: ElementInterfaceIntersect[] = []
+  layers: LottieLayer[] = []
   renderedFrame?: number
   tm?: ValueProperty
 
-  // constructor() {
-  //   super()
-  //   this.initHierarchy = new HierarchyElement().initHierarchy
-  // }
-
-  destroy() {
+  buildAllItems() {
+    throw new Error(
+      `${this.constructor.name}: Method buildAllItems is not implemented`
+    )
+  }
+  override destroy() {
     this.destroyElements()
     this.destroyBaseElement()
-  }
-  destroyBaseElement() {
-    throw new Error(
-      `${this.constructor.name}: Method destroyBaseElement is not implemented`
-    )
   }
   destroyElements() {
     const { length } = this.layers || []
     for (let i = 0; i < length; i++) {
-      if (this.elements?.[i]) {
+      if (this.elements[i]) {
         this.elements[i].destroy()
       }
     }
   }
+
   getElements(): ElementInterfaceIntersect[] | undefined {
     return this.elements
   }
 
-  hide() {
-    throw new Error(`${this.constructor.name}: Method hide is not implemented`)
-  }
-
-  initElement(
+  override initElement(
     data: LottieLayer,
     globalData: GlobalData,
     comp: ElementInterfaceIntersect
@@ -66,34 +49,63 @@ export default class CompElement extends BaseRenderer {
     this.initRendererElement()
     this.createContainerElements()
     this.createRenderableComponents()
-    if (this.data?.xt || !globalData.progressiveLoad) {
+    if (!this.data) {
+      throw new Error(
+        `${this.constructor.name}: data (LottieLayer) is not implemented`
+      )
+    }
+    if (this.data.xt || !globalData.progressiveLoad) {
       this.buildAllItems()
     }
     this.hide()
   }
 
-  initRenderable() {
-    throw new Error(
-      `${this.constructor.name}: Method initRenderable not implemented`
-    )
+  override prepareFrame(val: number) {
+    this._mdf = false
+    this.prepareRenderableFrame(val)
+    this.prepareProperties(val, this.isInRange)
+    if (!this.isInRange && !this.data?.xt) {
+      return
+    }
+
+    if (this.tm?._placeholder) {
+      this.renderedFrame = val / Number(this.data?.sr)
+    } else {
+      let timeRemapped = this.tm?.v
+      if (timeRemapped === this.data?.op) {
+        timeRemapped = Number(this.data?.op) - 1
+      }
+      this.renderedFrame = Number(timeRemapped)
+    }
+    const { length } = this.elements || []
+    if (!this.completeLayers) {
+      if (!this.checkLayers) {
+        throw new Error(
+          `${this.constructor.name}: Method checkLayers is not implemented`
+        )
+      }
+      this.checkLayers(this.renderedFrame)
+    }
+    // This iteration needs to be backwards because of how expressions connect between each other
+    for (let i = length - 1; i >= 0; i--) {
+      if (this.completeLayers || this.elements?.[i]) {
+        this.elements?.[i].prepareFrame?.(
+          this.renderedFrame - Number(this.layers?.[i].st)
+        )
+        if (this.elements?.[i]._mdf) {
+          this._mdf = true
+        }
+      }
+    }
   }
 
-  prepareFrame(_val: number) {
-    throw new Error(
-      `${this.constructor.name}: Method prepareFrame not implemented`
-    )
-  }
-
-  prepareRenderableFrame(_val: number, _?: boolean) {
-    throw new Error(
-      `${this.constructor.name}: Method prepareRenderableFrame not implemented`
-    )
-  }
-
-  renderInnerContent() {
-    throw new Error(
-      `${this.constructor.name}: Method renderInnerContent not implemented`
-    )
+  override renderInnerContent() {
+    const { length } = this.layers || []
+    for (let i = 0; i < length; i++) {
+      if (this.completeLayers || this.elements[i]) {
+        this.elements[i].renderFrame()
+      }
+    }
   }
 
   setElements(elems: ElementInterfaceIntersect[]) {
@@ -101,55 +113,20 @@ export default class CompElement extends BaseRenderer {
   }
 }
 
-extendPrototype(
-  [TransformElement, HierarchyElement, FrameElement, RenderableDOMElement],
-  CompElement
-)
+// const props = Object.getOwnPropertyNames(TransformElement.prototype),
+//   sorted = props.sort((a, b) => {
+//     if (a < b) {
+//       return -1
+//     }
+//     if (a > b) {
+//       return 1
+//     }
+//     return 0
+//   })
 
-CompElement.prototype.prepareFrame = function (val: number) {
-  this._mdf = false
-  this.prepareRenderableFrame(val)
-  this.prepareProperties(val, this.isInRange)
-  if (!this.isInRange && !this.data?.xt) {
-    return
-  }
-
-  if (this.tm?._placeholder) {
-    this.renderedFrame = val / Number(this.data?.sr)
-  } else {
-    let timeRemapped = this.tm?.v
-    if (timeRemapped === this.data?.op) {
-      timeRemapped = Number(this.data?.op) - 1
-    }
-    this.renderedFrame = Number(timeRemapped)
-  }
-  const { length } = this.elements || []
-  if (!this.completeLayers) {
-    if (!this.checkLayers) {
-      throw new Error(
-        `${this.constructor.name}: Method checkLayers is not implemented`
-      )
-    }
-    this.checkLayers(this.renderedFrame)
-  }
-  // This iteration needs to be backwards because of how expressions connect between each other
-  for (let i = length - 1; i >= 0; i--) {
-    if (this.completeLayers || this.elements?.[i]) {
-      this.elements?.[i].prepareFrame?.(
-        this.renderedFrame - Number(this.layers?.[i].st)
-      )
-      if (this.elements?.[i]._mdf) {
-        this._mdf = true
-      }
-    }
-  }
-}
-
-CompElement.prototype.renderInnerContent = function () {
-  const { length } = this.layers || []
-  for (let i = 0; i < length; i++) {
-    if (this.completeLayers || this.elements?.[i]) {
-      this.elements?.[i].renderFrame()
-    }
-  }
-}
+// for (const s of sorted) {
+//   if (s === 'constructor') {
+//     continue
+//   }
+//   console.log(s)
+// }
