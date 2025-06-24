14f61c3e70986e58f016b4481ea1cb8052e8ade2
Johan Martin Aarstein <johan@aarstein.media>
2025-06-24 03:35:13 +0200

    pkg

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,7 +2,7 @@ import type { Plugin, RollupOptions } from 'rollup'
 
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import path from 'node:path'
+import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { dts } from 'rollup-plugin-dts'
 import livereload from 'rollup-plugin-livereload'
@@ -13,7 +13,7 @@ import { typescriptPaths } from 'rollup-plugin-typescript-paths'
 
 const isProd = process.env.NODE_ENV !== 'development',
   { url } = import.meta,
-  __dirname = path.dirname(fileURLToPath(url)),
+  __dirname = dirname(fileURLToPath(url)),
   toPascalCase = (str: string) => {
     // Use regex to match words regardless of delimiter
     const words = str.match(/[A-Z]{2,}(?=[A-Z][a-z]+\d|\b)|[A-Z]?[a-z]+\d*|[A-Z]|\d+/g)
@@ -54,39 +54,41 @@ const isProd = process.env.NODE_ENV !== 'development',
   ],
   inputs = [
     {
-      file: path.resolve(
+      file: resolve(
         __dirname, 'src', 'Lottie.ts'
       ),
       name: 'lottie'
     },
     {
-      file: path.resolve(
+      file: resolve(
         __dirname, 'src', 'LottieLight.ts'
       ),
       name: 'lottie-light',
     },
     {
-      file: path.resolve(
+      file: resolve(
         __dirname, 'src', 'LottieUtils.ts'
       ),
       name: 'lottie-utils',
     },
     {
-      file: path.resolve(
+      file: resolve(
         __dirname, 'src', 'Dotlottie.ts'
       ),
       name: 'dotlottie'
     }
   ],
   types: RollupOptions[] = inputs.map((input) => ({
-    input: path.resolve(
+    input: resolve(
       __dirname, 'types', `${toPascalCase(input.name)}.d.ts`
     ),
     output: {
-      file: `./dist/${input.name}.d.ts`,
+      file: resolve(
+        __dirname, 'dist', `${input.name}.d.ts`
+      ),
       format: 'esm',
     },
-    plugins: [dts()],
+    plugins: [typescriptPaths(), dts()],
   })),
   outputs: RollupOptions[] = inputs.map((input) => ({
     external: [
@@ -101,7 +103,9 @@ const isProd = process.env.NODE_ENV !== 'development',
     },
     output: {
       exports: 'named',
-      file: `./dist/${input.name}.js`,
+      file: resolve(
+        __dirname, 'dist', `${input.name}.js`
+      ),
       format: 'esm',
     },
 
